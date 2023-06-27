2f5a56020b8a3c672cd06837b7260ff58c265b07
montuchavda123 <montuchavda456@gmail.com>
2023-06-27 08:48:10 +0530

    first commit

---
+++ file1.py
@@ -0,0 +1,16 @@
+
+num1=int(input("enter your first number"))
+num2=int(input("enter your second number"))
+
+sums=num1+num2
+subs=num1+num2
+multi=num1*num2
+divs=num1/num2
+module=num1%num2
+print("the sum of {0} and {1} is {2}=".format(num1,num2,sums))
+print("the subtraction of {0} and {1} is {2}=".format(num1,num2,subs))
+print("the mutliplication of {0} and {1} is {2}=".format(num1,num2,multi))
+print("the division of {0} and {1} is {2}=".format(num1,num2,divs))
+print("the moduler of {} and {} is {}=".format(num1,num2,module))
+
+
